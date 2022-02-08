f8acf4d87f1a8a56de7b4d97d2492583f3308b5c
aminLuis <aminluisamin@gmail.com>
2022-02-08 15:49:55 -0500

    Inicio aplicacion - plantilla implementada y segmentada - rutas definidas para componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { RegistroComponent } from './views/producto/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    BodyComponent,
+    RegistroComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {path: 'producto-registro', component: RegistroComponent}
+    ]),
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
