9eb73db7c0ea401d20bc53eed5287f50f1354d65
aminLuis <aminluisamin@gmail.com>
2022-02-28 16:38:00 -0500

    Servicio insumo implementado en modulo Insumo

--- src/app/views/insumo/insumo.component.ts
+++ src/app/views/insumo/insumo.component.ts
@@ -14,6 +14,7 @@ import { ApiInsumoService } from 'src/app/services/api-insumo.service';
 export class InsumoComponent implements OnInit {
 
   inventarios: Inventario[] = [];
+  insumos: Insumo[] = [];
   form_insumo: FormGroup;
   @Input() insumo!: Insumo;
  
@@ -40,18 +41,67 @@ export class InsumoComponent implements OnInit {
     
   }
 
+  listar_productos(){
+    this.api_insumo.getInsumos().subscribe(data =>{
+    this.insumos = data;
+    });
+  }
+
   save_insumo(){
     if(!this.form_insumo.valid){
       this.mensaje_error('Los campos deben estar llenos');
     }else{
-    
+      this.api_insumo.saveInsumo(this.form_insumo.value).subscribe();
+      this.mensaje('Se ha registrado el insumo!');
+      this.form_insumo.reset();
     }
+  }
+
+  public update_insumo(){
+    this.api_insumo.updateInsumo(this.insumo).subscribe();
+    this.mensaje('Se ha actualizado el insumo!')
+  }
+
+
+  delete_insumo(id:BigInteger){
+    Swal.fire({
+      title: '¿Seguro que desea eliminar el registro?',
+      text: "El registro se eliminará permanentemente",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, deseo eliminarlo!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.api_insumo.deleteInsumo(id).subscribe();
+        Swal.fire(
+          'Eliminado!',
+          'El registro ha sido eliminado.',
+          'success'
+        )
+      }
+    })
+  
+  }
+
+
+  cargar_datos(data: Insumo){
+    this.insumo = data;
+    console.log(this.insumo);
   }
 
 
 
-
-
+  mensaje(texto: string){
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: texto,
+      showConfirmButton: false,
+      timer: 1800
+    })
+  }
 
   mensaje_error(texo:string){
     Swal.fire({
