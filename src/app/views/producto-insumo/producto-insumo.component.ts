3a7dbe7531e2013f54e39fe015bbe258a232f597
aminLuis <aminluisamin@gmail.com>
2022-03-10 16:59:23 -0500

    Formulario para guardar Producto-insumo funcionando

--- src/app/views/producto-insumo/producto-insumo.component.ts
+++ src/app/views/producto-insumo/producto-insumo.component.ts
@@ -3,8 +3,11 @@ import { Producto } from 'src/app/models/Producto.interface';
 import { ApiProductoService } from 'src/app/services/api-producto.service';
 import { Insumo } from 'src/app/models/Insumo.interface';
 import { ApiInsumoService } from 'src/app/services/api-insumo.service';
-import { Observable } from 'rxjs';
-import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiProductoInsumoService } from 'src/app/services/api-producto-insumo.service';
+import Swal from 'sweetalert2';
+import { Subscription } from 'rxjs';
+import { Producto_insumo } from 'src/app/models/Producto_insumo.interface';
 
 @Component({
   selector: 'app-producto-insumo',
@@ -18,10 +21,13 @@ export class ProductoInsumoComponent implements OnInit {
   @Input() producto!:Producto;
   insumos: Insumo[]=[];
   @Input() data_insumo!:Insumo;
+  @Input() subscription!: Subscription;
+  productos_insumo: Producto_insumo[]=[];
 
   constructor(private api_producto:ApiProductoService, 
     private api_insumo:ApiInsumoService,
-    public formulario: FormBuilder
+    public formulario: FormBuilder,
+    private api_producto_insumo:ApiProductoInsumoService
     ) {
       this.form_producto_insumo = formulario.group({
         id_producto:[''],
@@ -32,6 +38,10 @@ export class ProductoInsumoComponent implements OnInit {
 
   ngOnInit(): void {
     this.listar_productos();
+    this.subscription = this.api_producto_insumo.reload.subscribe(()=>{
+      this.listar_productos_insumo();
+      this.listar_productos();
+    });
   }
 
   listar_productos(){
@@ -46,12 +56,46 @@ export class ProductoInsumoComponent implements OnInit {
     })
   }
 
+  listar_productos_insumo(){
+    this.api_producto_insumo.getProductos_insumo().subscribe(data=>{
+      this.productos_insumo = data;
+    })
+  }
+
   save_producto_insumo(){
-    console.log(this.form_producto_insumo.value)
+    if(this.form_producto_insumo.valid){
+      this.api_producto_insumo.saveProducto_insumo(this.form_producto_insumo.value).subscribe();
+      this.mensaje('Se ha registrado la asignación');
+      this.form_producto_insumo.reset();
+    }else{
+      this.mensaje_error('Los campos deben estar llenos');
+    }
   }
 
   cargar_datos_producto(data:Producto){
     this.producto = data;
   }
+
+
+
+  mensaje(texto: string){
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: texto,
+      showConfirmButton: false,
+      timer: 1800
+    })
+  }
+
+  mensaje_error(texo:string){
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: texo,
+      //footer: '<a href="">Why do I have this issue?</a>'
+    })
+  }
+
 
 }
