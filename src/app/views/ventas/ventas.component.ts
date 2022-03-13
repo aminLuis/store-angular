d08f544d4e2396414fe31abfe81ccafe9395bfec
aminLuis <aminluisamin@gmail.com>
2022-03-13 17:31:08 -0500

    Listado productos en componente Ventas

--- src/app/views/ventas/ventas.component.ts
+++ src/app/views/ventas/ventas.component.ts
@@ -9,11 +9,18 @@ import { ApiProductoService } from 'src/app/services/api-producto.service';
 })
 export class VentasComponent implements OnInit {
 
-  
+  productos: Producto[]=[];
 
   constructor(private api_producto:ApiProductoService) { }
 
   ngOnInit(): void {
+    this.listar_productos();
+  }
+
+  listar_productos(){
+    this.api_producto.getProductos().subscribe(data=>{
+      this.productos = data;
+    });
   }
 
 }
